a0098a93f9f9499ba0f02ad3577ffc7c718ba413
Lucas Correia <lucas.correia@softplan.com.br>
2019-08-21 17:47:37 -0300

    Updates

---
+++ precache-manifest.11a42d88bb3e9772ad2479aac16aa4b2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cfdbb56e4c3b71116c3e924cbf2d530",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd7c8e90d69df830b484",
+    "url": "/static/js/2.9ec55788.chunk.js"
+  },
+  {
+    "revision": "372dd40ddee24b106c33",
+    "url": "/static/js/main.1f13acd2.chunk.js"
+  },
+  {
+    "revision": "a3e4bc20457002584c92",
+    "url": "/static/js/runtime~main.efda3fbf.js"
+  }
+]);
